a21d46ed1b32d2f02dc66c7cc39c19a3fdff1997
vien pham <vienpham2019@gmail.com>
2019-12-19 22:14:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,7 +7,7 @@ def play(song)
   puts "Please enter a song name or number:"
   input = gets.strip
   if input.to_i < song.length
-    puts "Playing #{song(input.to_i)}"
+    puts "Playing #{song[input.to_i]}"
   elsif song.include?(input)
     puts "Playing #{input}"
   else
