3ec1853aad105c57944d49e702f6015c7cb3fb23
vien pham <vienpham2019@gmail.com>
2019-12-19 23:04:54 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,7 +20,7 @@ def play(song)
   binding.pry
   puts "Please enter a song name or number:"
   input = gets.strip
-  if input.to_i < song.length
+  if input < 2
     puts "Playing #{song[input.to_i]}"
   elsif song.include?(input)
     puts "Playing #{input}"
